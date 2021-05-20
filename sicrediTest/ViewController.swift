94ee78a70cfcae37d01a5c144a5603c3cbe39a20
Rita <ritadecassia.vasconcelos@hotmail.com>
2021-05-19 21:51:10 -0400

    Initial commit

---
+++ sicrediTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sicrediTest
+//
+//  Created by Rita de Cássia Vasconcelos do Nascimento on 19/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
